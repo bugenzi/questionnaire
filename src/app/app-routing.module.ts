8803fb1abbfbd9814d7833b8341465bbdf2f2886
bugenzi <amarbugarin@outlook.com>
2021-11-08 13:22:08 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PostComponent } from './pages/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '/', component: HomeComponent },
+    {
+        path: 'question',
+        component: PostComponent,
+        children: [
+            {
+                path: 'question/:id', //:type is dynamic here
+                component: PostComponent,
+            },
+        ],
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
